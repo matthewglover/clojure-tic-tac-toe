f802907acb0b2ae880f0a6e6eaea118cd4ee2711
Matt Glover <matthewglover@me.com>
2017-10-24 08:58:11 +0100

    Update human player UI

--- spec/clojure_ttt/ui/human_player_spec.clj
+++ spec/clojure_ttt/ui/human_player_spec.clj
@@ -1,4 +1,4 @@
-(ns clojure-ttt.human-player-spec
+(ns clojure-ttt.ui.human-player-spec
   (:require [speclj.core :refer :all]
             [clojure-ttt.ui.human-player :refer :all]))
 
@@ -7,19 +7,39 @@
 
 (describe "prompt-for-move"
           (it "prints move request prompt"
-              (should= str-move-request
-                        (with-out-str
-                          (with-in-str "\n"
+              (with-in-str "\n"
+                        (should= str-move-request
+                          (with-out-str
                             (prompt-for-move failing-validator)))))
 
           (context "with a valid input"
-                   (it "returns square number as an integer"
-                       (should= 5
-                                (with-in-str "5\n"
+              (it "returns square number as an integer"
+                       (with-in-str "5\n"
+                                (should= 5
                                   (prompt-for-move passing-validator)))))
           
           (context "with invalid input"
-                   (it "returns nil"
-                       (should= nil
-                                (with-in-str "failing input\n"
+              (it "returns nil"
+                       (with-in-str "failing input\n"
+                                (should= nil
                                   (prompt-for-move failing-validator))))))
+
+(def test-moves '(1 2 3))
+
+(describe "input-validator"
+          (context "non integer input"
+                   (it "returns false"
+                       (should-not
+                         (input-validator test-moves "non integer text"))))
+
+          (context "move taken input"
+                   (it "returns false"
+                       (should-not
+                         (input-validator test-moves "1")))))
+
+(describe "get-human-move"
+          (context "human inputs valid move"
+              (it "returns move as an integer"
+                        (with-redefs [prompt-for-move (constantly 1)]
+                            (should= 1
+                                  (get-human-move test-moves))))))
--- src/clojure_ttt/ui/human_player.clj
+++ src/clojure_ttt/ui/human_player.clj
@@ -2,18 +2,29 @@
 
 (def str-move-request "Enter your move: ")
 
-(defn- print-move-request
-  []
-  (print str-move-request)
-  (flush))
+(defn- is-free-square [moves square]
+  (not (some #(= square %) moves)))
+  
+(defn- is-parsable-integer [input]
+  (re-matches #"^\d$" input))
 
-(defn- get-user-input
-  [input-validator]
+(defn input-validator [moves input]
+  (and 
+    (is-parsable-integer input)
+    (is-free-square moves (Integer/parseInt input))))
+
+(defn- get-user-input [input-validator]
   (let [input (read-line)]
     (if (input-validator input)
       (Integer/parseInt input))))
 
-(defn prompt-for-move
-  [input-validator]
+(defn- print-move-request []
+  (print str-move-request)
+  (flush))
+
+(defn prompt-for-move [input-validator]
   (print-move-request)
   (get-user-input input-validator))
+
+(defn get-human-move [moves]
+  (prompt-for-move (partial input-validator moves)))
