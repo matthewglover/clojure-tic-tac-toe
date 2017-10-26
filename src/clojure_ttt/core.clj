38e16d1f60832505a889500611f66fa692f1c709
Matt Glover <matthewglover@me.com>
2017-10-26 10:39:34 +0100

    Extract game-runner from core

--- src/clojure_ttt/core.clj
+++ src/clojure_ttt/core.clj
@@ -1,17 +1,5 @@
 (ns clojure-ttt.core
-  (:require [clojure-ttt.ui.human-player :refer [get-human-move]]
-            [clojure-ttt.ui.game :refer [print-result]]
-            [clojure-ttt.board.moves :refer [update-moves]])
-  (:require clojure-ttt.game.status)
-  (:refer clojure-ttt.game.status :rename {over? game-over?}))
-
-(defn run-game []
-  (loop [moves []]
-    (let [move (get-human-move moves)
-          moves (update-moves moves move)]
-      (if (game-over? moves)
-        (print-result moves)
-        (recur moves))))) 
+  (:require [clojure-ttt.game.runner :refer [run-game]]))
 
 (defn -main [& args]
   (run-game))
---
+++ src/clojure_ttt/game/runner.clj
@@ -0,0 +1,14 @@
+(ns clojure-ttt.game.runner
+  (:require [clojure-ttt.ui.human-player :refer [get-human-move]]
+            [clojure-ttt.ui.game :refer [print-result]]
+            [clojure-ttt.board.moves :refer [update-moves]])
+  (:require clojure-ttt.game.status)
+  (:refer clojure-ttt.game.status :rename {over? game-over?}))
+
+(defn run-game []
+  (loop [moves []]
+    (let [move (get-human-move moves)
+          moves (update-moves moves move)]
+      (if (game-over? moves)
+        (print-result moves)
+        (recur moves))))) 
